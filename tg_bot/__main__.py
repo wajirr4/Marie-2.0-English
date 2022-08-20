e3a4378e59a536a0236a4ff93e4e37af6a1d603e
wajirr4 <109944533+wajirr4@users.noreply.github.com>
2022-08-21 01:35:00 +0600

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -45,7 +45,7 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/BotMakerBD) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!."""
 
